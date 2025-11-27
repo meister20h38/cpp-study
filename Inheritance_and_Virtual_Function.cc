1fb3d13f2783d2adffd52bcc566689fbb6b6c2ae
meister20h38 <fujimon40k76@gmail.com>
2025-11-27 13:00:02 +0900

    Implement Circle class for shape area calculation
    Added Circle class and implemented area calculation.

--- Inheritance_and_Virtual_Function.cc
+++ Inheritance_and_Virtual_Function.cc
@@ -1,3 +1,8 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 class Shape {
 public:
 Shape () {}
@@ -17,3 +22,22 @@ double area() override {
   return 3.14 * r * r;
 }
 };
+
+int main() {
+  vector<Shape*> shapes;
+  shapes.push_back(new Circle(10.0));
+  shapes.push_back(new Circle(20.0));
+
+  double total = 0.0;
+  for (const auto e : shapes) {
+    total += e->area();
+    }
+  
+  cout << total << endl;
+
+  for (auto e : shapes) {
+    delete e;
+    }
+
+return 0;
+}
